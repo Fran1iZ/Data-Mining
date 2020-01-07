70046889d014a2fc191f04d3341f91f811efc796
Fran1iZ <francis.carujo@gmail.com>
2020-01-07 01:44:43 +0000

    Previous files cleanup

--- Data_Clean.py
+++ Data_Clean.py
@@ -4,31 +4,21 @@ Data Mining
 2019/2020
 """
 
-__author__ = "Francisco Neves, Francisco Jorge and Pedro Carmona"
+__author__ = "Francisco Neves"
 __version__ = "1.0.0"
 #Invalid Data
-#Some birthdays are after the first policy
+
+#some birthdays are after the first policy year
+#assumed it was human error so the table is dropped
+
+df_engage.drop('Birthday',axis = 1, inplace= True)
 
 # Nulls
 
 df_engage = df_engage.dropna()
 df_lob = df_lob.dropna()
 
-""" Tests
-print(df_engage.isnull().sum())
-print(df_lob.isnull().sum())
-
-print("oi")
-print(len(df_lob))
-print(len(df_lob.dropna()))
-print((len(df_lob)-len(df_lob.dropna()))/len(df_lob)*100)
-"""
-
-#some birthdays are after the first policy year
-#assumed it was human error
-
-df_engage.drop('Birthday',axis = 1, inplace= True)
-
+#Calculate % (RECHECK)
 
 #engage
 #total - 10296
@@ -42,10 +32,24 @@ df_engage.drop('Birthday',axis = 1, inplace= True)
 #tot nulls - 317
 #tot cleaned - 1.54%
 
+""" Tests
+print(df_engage.isnull().sum())
+print(df_lob.isnull().sum())
+
+print("oi")
+print(len(df_lob))
+print(len(df_lob.dropna()))
+print((len(df_lob)-len(df_lob.dropna()))/len(df_lob)*100)
+"""
+
+
 # Outliers
 
 #engage
 
+#Checking outliers using boxplots
+
+"""
 #sns.boxplot(x=df_engage['First_Policy_Year'])
 # remove values aboxe 1999 - 1 val
 
@@ -59,49 +63,38 @@ df_engage.drop('Birthday',axis = 1, inplace= True)
 # remove above 1.56 - 4 vals
 
 #tot - 24 values (may have duplicates)
+"""
 
 engage_outliersList = df_engage.loc[df_engage['First_Policy_Year']>1999]
-"""
-print("FPY")
-print(df_engage.loc[df_engage['First_Policy_Year']>1999])
 
-print("SAL")
-print(df_engage.loc[df_engage['Salary']>5025])
-
-print("CVAL+")
-print(df_engage.loc[df_engage['Customer_Value']>2500])
-
-print("CVAL-")
-print(df_engage.loc[df_engage['Customer_Value']<-500])
-
-print("CLRAT")
-print(df_engage.loc[df_engage['Claims_Rate']>40])
-"""
 
 #check with others - temp thus far
 
 #500 > Value > 2500
 #Claim_Rate > 40
 
-#engage_outliersList = engage_outliersList.append(df_engage.loc[df_engage['First_Policy_Year']>1999]['c_ID'])
 
 engage_outliersList = engage_outliersList.append(df_engage.loc[df_engage['Salary']>5025])
 engage_outliersList = engage_outliersList.append(df_engage.loc[df_engage['Customer_Value']>2500])
 engage_outliersList = engage_outliersList.append(df_engage.loc[df_engage['Customer_Value']<-500])
 engage_outliersList = engage_outliersList.append(df_engage.loc[df_engage['Claims_Rate']>40])
 
-#print(engage_outliersList.drop_duplicates())
+
 engage_outliersList = engage_outliersList.drop_duplicates()
 # after clearing dups - 20 values
 
 df_engage_outliers = df_engage.copy()
 
-df_engage_outliers = df_engage_outliers.loc[~df_engage_outliers['c_ID'].isin(engage_outliersList['c_ID'])]
-#print(df_engage_outliers)
 #creates copy df without outliers
+df_engage_outliers = df_engage_outliers.loc[~df_engage_outliers['c_ID'].isin(engage_outliersList['c_ID'])]
+
 
 #lob
 
+
+#Checking outliers using boxplots
+
+"""
 #sns.boxplot(x=df_lob['Motor'])
 # remove values aboxe 590
 
@@ -116,6 +109,7 @@ df_engage_outliers = df_engage_outliers.loc[~df_engage_outliers['c_ID'].isin(eng
 
 #sns.boxplot(x=df_lob['Work_Compensation'])
 # remove above 360
+"""
 
 
 lob_outliersList = df_lob.loc[df_lob['Motor']>590]
@@ -123,10 +117,26 @@ lob_outliersList = df_lob.loc[df_lob['Motor']>590]
 
 lob_outliersList = lob_outliersList.append(df_lob.loc[df_lob['Household']>2000])
 lob_outliersList = lob_outliersList.append(df_lob.loc[df_lob['Health']>500])
-lob_outliersList = lob_outliersList.append(df_lob.loc[df_lob['Life']>370])
 lob_outliersList = lob_outliersList.append(df_lob.loc[df_lob['Work_Compensation']>360])
+
+""" Test Values
+lob_outliersList = df_lob.loc[df_lob['Motor']>2000]
+
+
+lob_outliersList = lob_outliersList.append(df_lob.loc[df_lob['Household']>2000])
+lob_outliersList = lob_outliersList.append(df_lob.loc[df_lob['Health']>500])
+lob_outliersList = lob_outliersList.append(df_lob.loc[df_lob['Work_Compensation']>750])
+"""
+
 
 lob_outliersList = lob_outliersList.drop_duplicates()
 df_lob_outliers=df_lob.copy()
 
+#creates copy df without outliers
 df_lob_outliers = df_lob_outliers.loc[~df_lob_outliers['c_ID'].isin(lob_outliersList['c_ID'])]
+
+
+#Clears index after outliers to aid in clustering
+df_lob_outliers.reset_index(drop=True,inplace=True)
+df_engage_outliers.reset_index(drop=True,inplace=True)
+
--- Pre-Processing.py
+++ Pre-Processing.py
@@ -2,6 +2,11 @@
 NOVA IMS
 Data Mining
 2019/2020
+
+Order of running
+(1) - Pre-Processing.py
+(2) - Data_Clean.py
+(3) - Either Lob.py or Enga.py for their respective clusters
 """
 __author__ = "Francisco Neves, Francisco Jorge and Pedro Carmona"
 __version__ = "1.0.0"
@@ -15,7 +20,6 @@ import sklearn
 from sklearn import preprocessing
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn import preprocessing
 from sklearn.cluster import KMeans
 import plotly as py
 import plotly.graph_objs as go
@@ -27,6 +31,12 @@ import sklearn.metrics as sm
 from sklearn.decomposition import PCA
 from kmodes import kmodes
 from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import silhouette_samples, silhouette_score
+from kmodes.kmodes import KModes
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+from sklearn import mixture
 import os
 
 
@@ -42,8 +52,7 @@ df_lob = pd.read_sql_query(query_lob, conn)
 df_engage = pd.read_sql_query(query_engage, conn)
 
 
-#Rename columns
-
+#Rename columns for easier handling
 df_lob.rename(index = str, columns = {'Customer Identity':'c_ID',
                                     'Premiums in LOB: Motor':'Motor',
                                     'Premiums in LOB: Household' : 'Household',
@@ -62,9 +71,11 @@ df_engage.rename(index = str, columns = {'Customer Identity':'c_ID',
                                       'Customer Monetary Value':'Customer_Value',
                                       'Claims Rate':'Claims_Rate'},inplace = True )
 
+#Clears Extra Index Column
 df_engage.drop(columns=['index'],inplace=True)
 df_lob.drop(columns=['index'],inplace=True)
 
+#Clean Educational Degree -> Convert previous string into a new integer for better analysis
 df_engage.loc[df_engage['Educational_Degree'] == '1 - Basic', 'Educational_Degree'] = int('1')
 df_engage.loc[df_engage['Educational_Degree'] == '2 - High School', 'Educational_Degree'] = int('2')
 df_engage.loc[df_engage['Educational_Degree'] == '3 - BSc/MSc', 'Educational_Degree'] = int('3')
